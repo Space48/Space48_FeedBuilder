56a94217ad354399a7441c181c4fe3cafedcff75
Colin Tickle <colin@space48.com>
2016-02-08 14:01:22 +0000

    Refactored code to move child product collection functions to correct place and handle iteration in the iterator model.

--- app/code/community/Space48/FeedBuilder/Model/DataIterator/ProductWithInheritance.php
+++ app/code/community/Space48/FeedBuilder/Model/DataIterator/ProductWithInheritance.php
@@ -10,75 +10,9 @@
 class Space48_FeedBuilder_Model_DataIterator_ProductWithInheritance
     extends Space48_FeedBuilder_Model_DataIterator_Abstract
 {
-    protected $_productTypeFilters = array('Space48_FeedBuilder_Model_Data_Filter_ProductsSimpleOrParent');
     protected $_childCollection;
     protected $_currentChildItemPosition;
     protected $_childItemReferences = array();
-
-    protected function _removeProductTypeFilter($feedConfig)
-    {
-        if (!isset($feedConfig['filters'])) {
-            return $feedConfig;
-        }
-
-        $filters = array();
-        foreach ($feedConfig['filters'] as $filterName => $filterConfig) {
-            if (!isset($filterConfig['class'])
-                || ! in_array($filterConfig['class'], $this->_productTypeFilters)) {
-                $filters[$filterName] = $filterConfig;
-            }
-        }
-        $feedConfig['filters'] = $filters;
-        return $feedConfig;
-    }
-
-    protected function _getChildDataModel()
-    {
-        if (!($dataModel = $this->_dataModel->getDataModel('class'))) {
-            Mage::throwException('Feed data model for child collection not defined');
-        } elseif (!class_exists($dataModel)) {
-            Mage::throwException('Feed data model for child collection does not exist :: ' . $dataModel);
-        } else {
-            $feedConfig = $this->_dataModel->getData();
-            $feedConfig = $this->_removeProductTypeFilter($feedConfig);
-            return new $dataModel($feedConfig);
-        }
-    }
-
-    protected function _getChildItem(Mage_Catalog_Model_Product $product)
-    {
-        // loading a new collection of products
-        if (! $this->_childCollection) {
-            $this->_currentChildItemPosition = 0;
-            $childDataModel = $this->_getChildDataModel();
-            $childrenIds = $product->getTypeInstance()->getChildrenIds($product->getId());
-            if(!$childrenIds) {
-                return false;
-            }
-            $childCollection =
-                Mage::getModel('catalog/product')
-                    ->getCollection()
-                    ->addIdFilter($childrenIds[0]);
-            $childDataModel->setCollection($childCollection);
-            $childDataModel->setItemsPerIteration(10000);
-            $this->_childCollection = $childDataModel->getIterationOfCollection();
-            $this->_setChildItemReferenceArray();
-        }
-
-        $this->_currentChildItemPosition++;
-        if (!$this->_childCollection) {
-            $item = false;
-        } else {
-            // Make the next loop call this function again.
-            if($this->_currentChildItemPosition < $this->_childCollection->count()) {
-                $this->_currentItemPosition--;
-            }
-
-            $item = $this->_childItemReferences[ $this->_currentChildItemPosition - 1];
-        }
-
-        return $item;
-    }
 
     protected function _setChildItemReferenceArray()
     {
@@ -86,6 +20,28 @@ class Space48_FeedBuilder_Model_DataIterator_ProductWithInheritance
         foreach ($this->_childCollection as &$item) {
             $this->_childItemReferences[] = $item;
         }
+    }
+
+    protected function _getChildItem(Mage_Catalog_Model_Product $parentProduct)
+    {
+        // loading a new collection of products
+        if (! $this->_childCollection) {
+            $this->_currentChildItemPosition = 0;
+            $this->_childCollection = $this->_dataModel->getChildItems($parentProduct);
+            $this->_setChildItemReferenceArray();
+        }
+
+        $this->_currentChildItemPosition++;
+        if (!$this->_childCollection) {
+            return false;
+        }
+
+        if($this->_currentChildItemPosition < $this->_childCollection->count()) {
+            $this->_currentItemPosition--;
+        }
+
+        $item = $this->_childItemReferences[ $this->_currentChildItemPosition - 1];
+        return $item;
     }
 
     public function getCollectionItem()

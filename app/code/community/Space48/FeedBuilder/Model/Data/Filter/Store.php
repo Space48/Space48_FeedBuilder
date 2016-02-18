8a5f3405969a19656f794cc8f956a0dd59081183
Che Such <che@space48.com>
2016-02-18 12:41:22 +0000

    Revert "#268 : fix for issue where both feeds were using the same shopregis url"
    This reverts commit e333f74cc59478178e4cd16f3743a5d8c0b1efa1.

--- app/code/community/Space48/FeedBuilder/Model/Data/Filter/Store.php
+++ app/code/community/Space48/FeedBuilder/Model/Data/Filter/Store.php
@@ -13,14 +13,7 @@ class Space48_FeedBuilder_Model_Data_Filter_Store extends Space48_FeedBuilder_Mo
             Mage::throwException('one of store id or store code must be supplied');
         }
 
-        $this->setDefaultStore($storeId);
-
         return $storeId;
-    }
-
-    protected function setDefaultStore($storeId)
-    {
-        Mage::app()->setCurrentStore($storeId);
     }
 
     public function addFilter(Varien_Data_Collection $collection)
